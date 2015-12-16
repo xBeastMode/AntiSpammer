eb1dffd78d958292e72862c22d50aafae92530c9
xBeastMode <xbeastmode00@gmail.com>
2015-12-16 15:34:19 -0800

    Update AntiSpammer.php

--- src/xbeastmode/antispammer/AntiSpammer.php
+++ src/xbeastmode/antispammer/AntiSpammer.php
@@ -1,24 +1,21 @@
 <?php
-
 namespace xbeastmode\antispammer;
-
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as color;
-
 class AntiSpammer extends PluginBase implements Listener
 {
     /** @var array */
-    private $m;
-    /** @var object */
-    private $p;
+    private $time;
     /** @var PlayerChatEvent */
     private $ev;
     /** @var array */
     private $x = [];
+    /** @var array */
+    private $spams = [];
     public function onEnable()
     {
         $this->getLogger()->info($this->par("%1%Plugin loaded!", color::GREEN));
@@ -44,24 +41,43 @@ class AntiSpammer extends PluginBase implements Listener
         }
         return $f;
     }
-    public function checkMessage()
+    /**
+     * checks message for any flooding
+     *
+     * @param Player $player
+     */
+    public function checkMessage(Player $player)
     {
-        if($this->isSpamming())
+        if($this->isSpamming() && !$player->isOp())
         {
-            $this->p->sendMessage($this->par("%1%Please wait 3 seconds before chatting again.", color::RED));
+            if(!isset($this->spams[$player->getName()]) && !$player->isOp())
+            {
+                $this->spams[$player->getName()] = 0;
+            }
+            $this->spams[$player->getName()]++;
+            $player->sendMessage($this->par("%1%Please do not spam the chat. Warnings: " . $this->spams[$player->getName()] . ".\n%2%* If your warnings are 3 you will be kicked.", color::RED, color::AQUA));
             $this->ev->setCancelled();
+            if($this->spams[$player->getName()] === 3)
+            {
+                $this->getServer()->broadcastMessage($this->par("%1%[Server] kicked %2%. Reason: spamming the chat.", color::RED, $player->getName()));
+                $player->kick($this->par("%1%\nSpamming the chat.", color::RED));
+                unset($this->spams[$player->getName()]);
+            }
         }
         else
         {
-            $this->m = time();
+            $this->time = time();
         }
     }
     /**
-     *@return bool 
+     * if the current time minus the time the message was sent
+     * is less than 2 seconds it will be considered spam
+     *
+     *@return bool
      */
     public function isSpamming()
     {
-        return (time() - $this->m < 3);
+        return (time() - $this->time < 2);
     }
     /**
      * @param PlayerChatEvent $e
@@ -71,6 +87,6 @@ class AntiSpammer extends PluginBase implements Listener
         $msg = $e->getMessage();
         $this->p = $e->getPlayer();
         $this->ev = $e;
-        $this->checkMessage();
+        $this->checkMessage($e->getPlayer());
     }
 }
