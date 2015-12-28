f3e20a95d879f514caf15753c2e796c51e494093
xBeastMode <xbeastmode00@gmail.com>
2015-12-28 00:02:19 -0800

    Update AntiSpammer.php

--- src/xbeastmode/antispammer/AntiSpammer.php
+++ src/xbeastmode/antispammer/AntiSpammer.php
@@ -1,93 +1,40 @@
 <?php
-namespace xbeastmode\antispammer;
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat as color;
-class AntiSpammer extends PluginBase implements Listener
-{
-    /** @var array */
-    private $time;
-    /** @var PlayerChatEvent */
-    private $ev;
-    /** @var array */
-    private $x = [];
-    /** @var array */
-    private $spams = [];
-    public function onEnable()
-    {
-        $this->getLogger()->info($this->par("%1%Plugin loaded!", color::GREEN));
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-    /**
-     * @param $str
-     * @return int
-     */
-    private function par(...$str)
-    {
-        $f = array_shift($str);
-        if(isset($this->x[$f])) $f = $this->x[$f];
-        if(count($str)) {
-            $p = ["%%" => "%"];
-            $int = 1;
-            foreach($str as $par)
-            {
-                $p["%nl%"] = "\n";
-                $p["%$int%"] = $par;
-                $int++;
-            }
-            $f = strtr($f, $p);
+namespace antispam;
+    use pocketmine\Player;
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\event\player\PlayerChatEvent;
+    use pocketmine\utils\TextFormat;
+    use pocketmine\event\Listener;
+    class AntiSpam extends PluginBase implements Listener{
+        private $players = [];
+        public function onEnable(){
+            $this->getLogger()->info(TextFormat::GREEN."[AntiSpam] Plugin loaded!");
+            $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->saveDefaultConfig();
         }
-        return $f;
-    }
-    /**
-     * checks message for any flooding
-     *
-     * @param Player $player
-     */
-    public function checkMessage(Player $player)
-    {
-        if($this->isSpamming() && !$player->isOp())
-        {
-            if(!isset($this->spams[$player->getName()]) && !$player->isOp())
-            {
-                $this->spams[$player->getName()] = 0;
+        public function sendMessage(Player $p){
+            if($p->isOp() || $p->hasPermission("spam.bypass")) return false;
+            if($this->timeCheck($p)){
+                $p->sendMessage(str_replace("%player%", $p->getName(),
+                    $this->getConfig()->get("message")));
+                return true;
             }
-            $this->spams[$player->getName()]++;
-            $player->sendMessage($this->par("%1%Please do not spam the chat. Warnings: " . $this->spams[$player->getName()] . ".%nl%%2%* If your warnings are 3 you will be kicked.", color::RED, color::AQUA));
-            $this->ev->setCancelled();
-            if($this->spams[$player->getName()] === 3)
-            {
-                $this->getServer()->broadcastMessage($this->par("%1%[Server] kicked %2%. Reason: spamming the chat.", color::RED, $player->getName()));
-                $player->kick($this->par("%1%%nl%Spamming the chat.", color::RED));
-                unset($this->spams[$player->getName()]);
+            else{
+                $this->players[$p->getName()] = time();
+                return false;
             }
         }
-        else
-        {
-            $this->time = time();
+        public function timeCheck(Player $p){
+            if(isset($this->players[$p->getName()])) {
+                if (time() - $this->players[$p->getName()] <= intval($this->getConfig()->get("time"))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public function onChat(PlayerChatEvent $e){
+            if($this->sendMessage($e->getPlayer())){
+                $e->setCancelled();
+            }
         }
     }
-    /**
-     * if the current time minus the time the message was sent
-     * is less than 2 seconds it will be considered spam
-     *
-     *@return bool
-     */
-    public function isSpamming()
-    {
-        return (time() - $this->time < 2);
-    }
-    /**
-     * @param PlayerChatEvent $e
-     */
-    public function onChat(PlayerChatEvent $e)
-    {
-        $msg = $e->getMessage();
-        $this->p = $e->getPlayer();
-        $this->ev = $e;
-        $this->checkMessage($e->getPlayer());
-    }
-}
