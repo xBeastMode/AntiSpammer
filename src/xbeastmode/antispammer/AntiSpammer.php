4c373d57640274a01b161862667aaea1157a7c56
xBeastMode <xbeastmode00@gmail.com>
2016-02-22 19:26:23 -0800

    optimize

--- src/xbeastmode/antispammer/AntiSpammer.php
+++ src/xbeastmode/antispammer/AntiSpammer.php
@@ -8,33 +8,19 @@ namespace antispam;
     class AntiSpam extends PluginBase implements Listener{
         private $players = [];
         public function onEnable(){
-            $this->getLogger()->info(TextFormat::GREEN."[AntiSpam] Plugin loaded!");
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
             $this->saveDefaultConfig();
         }
-        public function sendMessage(Player $p){
-            if($p->isOp() || $p->hasPermission("spam.bypass")) return false;
-            if($this->timeCheck($p)){
+        public function onChat(PlayerChatEvent $e){
+            if($p->isOp() || $p->hasPermission("spam.bypass")) return;
+            if(isset($this->players[spl_object_hash($p)]) and (time() - $this->players[$p->getName()] <= intval($this->getConfig()->get("time")))){
                 $p->sendMessage(str_replace("%player%", $p->getName(),
                     $this->getConfig()->get("message")));
                 return true;
             }
             else{
-                $this->players[$p->getName()] = time();
+                $this->players[spl_object_hash($p)] = time();
                 return false;
-            }
-        }
-        public function timeCheck(Player $p){
-            if(isset($this->players[$p->getName()])) {
-                if (time() - $this->players[$p->getName()] <= intval($this->getConfig()->get("time"))) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        public function onChat(PlayerChatEvent $e){
-            if($this->sendMessage($e->getPlayer())){
-                $e->setCancelled();
             }
         }
     }
